a6dd7461af72f797f85a4cf93152481ceea49257
Brian Doyle <briandoyle147@gmail.com>
2018-04-09 15:21:47 +0100

    Added Logos and wrapping to main page

--- SixNationsTracker/SixNationsTracker/MainPage.xaml.cs
+++ SixNationsTracker/SixNationsTracker/MainPage.xaml.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -26,5 +19,6 @@ namespace SixNationsTracker
         {
             this.InitializeComponent();
         }
+
     }
 }
