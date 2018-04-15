a6f9104c36e6ff366d0b38d150daae444e0cc421
Brian Doyle <briandoyle147@gmail.com>
2018-04-15 16:49:50 +0100

    Added connection to neo4j and basic test of the connection

--- SixNationsTracker/SixNationsTracker/IrelandPage.xaml.cs
+++ SixNationsTracker/SixNationsTracker/IrelandPage.xaml.cs
@@ -13,6 +13,13 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
+using System.Data;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Text;
+using Neo4jClient;
+using Neo4j.Driver.V1;
+
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace SixNationsTracker
@@ -22,9 +29,20 @@ namespace SixNationsTracker
     /// </summary>
     public sealed partial class IrelandPage : Page
     {
+        
         public IrelandPage()
         {
             this.InitializeComponent();
+
+            var client = new GraphClient(new Uri("http://localhost:7474/db/data"), "neo4j", "Rubydoy14");
+            client.Connect();
+
+            IEnumerable<string> results = client.Cypher
+            .Match("(p:Person)-[:ACTED_IN]->(m:Movie {title: 'Top Gun'})")
+            .Return<string>("p.name").Results;
+
+            lvCenter.ItemsSource = results;
+
         }
 
         private void TbReturn_Tapped(object sender, TappedRoutedEventArgs e)
